a4c65adff6d6f356b299c97aeb705ed9acb3dfb0
Alex Paul Kaufman & Kameron Ross Johnson <student@epicodus.com>
2015-01-12 11:42:06 -0800

    Completes specs and methods

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,11 +1,24 @@
 class Parcel
-  define_method(:initialize) do |length, width, height|
+  define_method(:initialize) do |length, width, height, weight|
     @length = length
     @width = width
     @height = height
+    @weight = weight
   end
 
   define_method(:volume) do
     @length.*(@width).*(@height)
+  end
+
+  define_method(:cost_to_ship) do
+    price = 0
+    price = price.+(@weight*(10))
+    if self.volume.<=(10)
+      price = price.+(2)
+    elsif self.volume.<=(20)
+      price = price.+(4)
+    else
+      price = price.+(6)
+    end
   end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -4,16 +4,16 @@ require('parcel')
 describe(Parcel) do
   describe('#volume') do
     it('tells you the volume of a parcel') do
-      test_parcel = Parcel.new(3, 3, 3)
+      test_parcel = Parcel.new(3, 3, 3, 1)
       expect(test_parcel.volume()).to(eq(27))
     end
   end
 
-  # describe('#volume') do
-  #   it('tells you the volume of a parcel') do
-  #     test_parcel = Parcel.new(3, 3, 3)
-  #     expect(test_parcel.volume()).to(eq(27))
-  #   end
-  # end
+  describe('#cost_to_ship') do
+    it('tells you the cost to ship that parcel') do
+      test_parcel = Parcel.new(3, 3, 3, 1)
+      expect(test_parcel.cost_to_ship()).to(eq(16))
+    end
+  end
 
 end
